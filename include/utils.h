96acac5e3627391a249013011fe04e65f6a1cbde
manniq31 <mannip.31@gmail.com>
2019-03-17 21:50:11 +0100

    added comments

--- include/utils.h
+++ include/utils.h
@@ -16,20 +16,68 @@ char menu(char *text, int options);
  * ----------------------------
  *  parses single character yes or no input (Y or N)
  *  user input is not case sensitive
+ * the promt shows the default option in upper case
  *
- *  def: (0|1) default return value that is used if the user just presses enter
+ *  def: (true|false) default return value that is used if the user just presses enter
  *
- *  returns: 1 for 'y', 0 for 'n', <def> for 'enter'
+ *  returns: true for 'y', false for 'n', <def> for 'enter'
  */
 bool yesno(bool def);
-void printLib();
+
+/*
+ * Function: printlib
+ * ----------------------------
+ *  prints the given library to the console
+ * 
+ *  plib: pointer to the library that should be printed
+ */
+void printLib(library *pLib);
+
+/*
+ * Function: printBook
+ * ----------------------------
+ *  prints a single book
+ */
 void printBook(book *book);
+
+/*
+ * Function: clearConsole
+ * ----------------------------
+ *  clears any ramining output from the console
+ */
 void clearConsole();
+
+/*
+ * Function: clearInput
+ * ----------------------------
+ *  clears the input buffer
+ */
 void clearInput();
+
 bool isbnValidation();
-//takes user input of length 'length' and stores it into 'buffer'
+
+/*
+ * Function: getString
+ * ----------------------------
+ * takes user input of length 'length' and stores it into 'buffer'
+ * 
+ * buffer: pointer to the area where the input should be stored
+ * length: max length that the user input can have
+ * 
+ * returns: false if succesfull, true if input was to long
+ */
 bool getString(char *buffer, int length);
-//converts a string to uppercase
+
+
+/*
+ * Function: upperString
+ * ----------------------------
+ * takes a string an stores it as upper case
+ * source and destination can savely be the same 
+ * 
+ * dest: pointer to the location where the upper case string is stored
+ * src: pointer to the given string
+ */
 void upperString(char *dest, char *src);
 
 #endif
